8f61726ae8ca3e1222410d57be825f95abbac563
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-09-22 17:46:18 +0530

    updated api url

--- src/Api/userApi.ts
+++ src/Api/userApi.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { notification } from "antd";
 
-const baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:5000" : "";
+const baseURL:string =
+  process.env.NODE_ENV === "development" ? "http://localhost:5000" : "https://code-store-back.vercel.app";
 
 export const ApiFunc = axios.create({ baseURL: baseURL });
 
